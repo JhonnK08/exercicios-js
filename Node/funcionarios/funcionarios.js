8395cf5a7f694e90ea65ef37281f3114c98d244e
Jhonatan <38366068+JhonnK08@users.noreply.github.com>
2021-02-25 20:17:18 -0300

    desafio

--- Node/funcionarios/funcionarios.js
+++ Node/funcionarios/funcionarios.js
@@ -1,7 +1,27 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const chineses = f => f.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
 axios.get(url).then(response => {
     const funcionarios = response.data
-    console.log(funcionarios)
+    //console.log(funcionarios)
+    const func = funcionarios.filter(chineses).filter(mulheres).reduce(menorSalario)
+    console.log(func)
 })
+
+function menorSalarioMulherChina(funcionarios) {
+    const obj = {}
+    obj = JSON.parse(funcionarios)
+    const chineses = obj.filter(objeto => objeto.pais === 'China')
+    const mulheresChinesas = chineses.filter(objeto => objeto.genero === 'F')
+    const maior = mulheresChinesas[0]
+    const menorSalarioMulher = mulheresChinesas.reduce((maior, mulher) => {
+        if(maior.salario < mulher.salario)
+            maior = mulher
+    })
+    return menorSalarioMulher
+}
